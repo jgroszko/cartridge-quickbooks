6ab47dc1b296dace107db4350e2f89f838dea440
John Groszko <john@tinythunk.com>
2016-04-15 19:50:15 -0500

    Handle unauthorized exception better

--- quickbooks/views.py
+++ quickbooks/views.py
@@ -10,6 +10,14 @@ from quickbooks.payments import Payments
 SESSION_ACCESS_TOKEN = 'qb_oauth_access_token'
 SESSION_ACCESS_SECRET = 'qb_oauth_access_secret'
 
+def _reauthorize(request, payments):
+    authorize_url = payments.get_auth_url(request.build_absolute_uri())
+
+    request.session[SESSION_ACCESS_TOKEN] = payments.access_token
+    request.session[SESSION_ACCESS_SECRET] = payments.access_secret
+
+    return authorize_url
+
 @staff_member_required
 def charges(request):
     data = {}
@@ -22,13 +30,13 @@ def charges(request):
                            request.GET['realmId'])
 
     if(hasattr(p, 'session')):
-        data['charges'] = p.charges()
+        try:
+            data['charges'] = p.charges()
+        except:
+            data['authorize_url'] = _reauthorize(request, p)
 
     else:
-        data['authorize_url'] = p.get_auth_url(request.build_absolute_uri())
-
-        request.session[SESSION_ACCESS_TOKEN] = p.access_token
-        request.session[SESSION_ACCESS_SECRET] = p.access_secret
+        data['authorize_url'] = _reauthorize(request, p)
 
     return render_to_response('quickbooks/charges.html',
                               data,
