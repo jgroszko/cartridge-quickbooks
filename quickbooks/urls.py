67b4a295df8e684f3a9f3c3c82f6863888fb07a2
John Groszko <john@tinythunk.com>
2016-10-31 22:17:30 -0500

    Upgrade to Django 1.10

--- quickbooks/urls.py
+++ quickbooks/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 
 from quickbooks.views import charges, charges_refund
 
-urlpatterns = patterns('',
+urlpatterns = [
                        url(r'^charges/$', charges, name="quickbooks_charges"),
                        url(r'^charges/refund/([A-Z0-9]{12})$', charges_refund, name="quickbooks_charges_refund")
-)
+]
 
